efdd860346e21983facc20c6b6f28b26afbc263e
igorminucelli <igor.minucelli@unifesp.br>
2026-04-21 20:18:41 -0300

    Fix return statement formatting

--- 2757.c
+++ 2757.c
@@ -17,5 +17,5 @@ printf("A = %d, B = %d, C = %d\n", A,B,C);
 printf("A = %10d, B = %10d, C = %10d\n", A,B,C);
 printf("A = %010d, B = %010d, C = %010d\n", A,B,C);
 printf("A = %-10d, B = %-10d, C = %-10d\n", A,B,C);
-return 0;
+ return 0;
 }
