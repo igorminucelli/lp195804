7019d46506bfbe5ef364a795f92bf94960d07b1e
igorminucelli <igor.minucelli@unifesp.br>
2026-04-21 20:15:55 -0300

    Add program to find max value and position from 100 inputs
    Implement logic to read 100 integers and find the maximum value and its position.

---
+++ 1080.c
@@ -0,0 +1,32 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Igor Aparecido Minucelli Pinto>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
+Data        : 19/04/2026
+Objetivo    : Leia 100 valores inteiros. Apresente então o maior valor lido e a posição dentre os 100 valores lidos.
+Aprendizado : <<<Aprendemos o uso do módulo >>>
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main(){
+    int X[100],Maior=-1,Posicao=-1,i;
+    
+    for(i=0; i<100; i++){
+        scanf("%d", &X[i]);
+        
+        if(X[i]<0){
+            return 0;
+        }
+        
+        if(X[i]>Maior){
+            Maior=X[i];
+            Posicao=i+1;
+        }
+    }
+    
+    printf("%d\n", Maior);
+    printf("%d\n", Posicao);
+    
+    return 0;
+}
