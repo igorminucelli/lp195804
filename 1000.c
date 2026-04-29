476efdd2ff55ef8fca41a55655d0f01f5b6ef448
igorminucelli <igor.minucelli@unifesp.br>
2026-04-29 20:30:37 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Igor Aparecido Minucelli Pinto>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : 07/04/2026
+Objetivo    : Escrever "Hello World!"
+Aprendizado : <<< Começo em linguagem C >>>
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main()
