997c62e4c24eb02cda1bc39951d500afb782e420
igorminucelli <igor.minucelli@unifesp.br>
2026-06-23 15:55:27 -0300

    Fix formatting and spacing in main function

--- 1013.c
+++ 1013.c
@@ -10,8 +10,7 @@ Aprendizado : <<< Fazer Somas em C >>>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
+int main(){
     int a,b,c,MaiorAB,MaiorFinal;
     
     scanf("%d %d %d",&a,&b,&c);
@@ -20,7 +19,7 @@ int main()
     
     MaiorFinal=(MaiorAB+c+abs(MaiorAB-c))/2;
     
-printf("%d eh o maior\n",MaiorFinal);
+    printf("%d eh o maior\n",MaiorFinal);
 
     return 0;
 }
